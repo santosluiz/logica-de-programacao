40dd212dca92b7cc54513d534b8ebbf91af14117
Luiz Santos <97luizsantos@gmail.com>
2017-03-14 11:02:38 -0300

    Done

--- questions/question6.c
+++ questions/question6.c
@@ -4,5 +4,16 @@
 // 6. Ler dois números inteiros e exibir o quociente e o resto da divisão inteira entre eles.
 
 main(){
+	int num, num2, quociente, resto;
 	
+	printf ("Digite os valores:\n");
+	scanf ("%d %d", &num, &num2);
+	
+	quociente = num / num2;
+	resto = num % num2;
+	
+	printf ("O quociente da divisao e: %d\n", quociente);
+	printf ("O resto da divisao e: %d\n\n", resto);
+	
+	return 0;
 }
