aa58dfb6c77d082c2d40cffc35c210dd7cbfdf5d
Luiz Santos <97luizsantos@gmail.com>
2017-09-26 00:10:16 -0300

    Done

--- questions/question_42.c
+++ questions/question_42.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 42. Escreva um programa que imprima todos os números inteiros de 100 a 1 (em ordem decrescente).
+// 42. Escreva um programa que imprima todos os números inteiros de 10 a 1 (em ordem decrescente).
 
 main(){
+	int vetor[10], i, j, aux;
 	
+	for(i=0; i<10; i++){
+		printf("Digite um numero:");
+		scanf("%d", &vetor[i]);
+	}
+	
+	for(i=0; i<10; i++){	
+		for(j=i+1; j<10; j++){		
+		
+			if(vetor[i] < vetor[j]){		
+				aux = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = aux;			
+			}
+		}
+	}
+
+	for(i=0; i<10; i++){
+		printf("%d\n", vetor[i]);
+	} 	
 }
