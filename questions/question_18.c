fcdbdae526b2d77201384ab010d8478991349444
Luiz Santos <97luizsantos@gmail.com>
2017-09-25 23:30:37 -0300

    Done

--- questions/question_18.c
+++ questions/question_18.c
@@ -4,5 +4,17 @@
 //18. Escreva um programa que permute o valor de duas variáveis inteiras.
 
 main(){
+	int a, b, aux;
 	
+	printf("Digite o numero A:");
+	scanf("%d", &a);
+	
+	printf("Digite o numero B:");
+	scanf("%d", &b);	
+	
+	aux = a;
+	a = b;
+	b = aux;
+	
+	printf("Valor de A: %d \nValor de B: %d", a, b);	
 }
