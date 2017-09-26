46ecb13738b4a0a732eabf5e149eb3cd87a1573c
Luiz Santos <97luizsantos@gmail.com>
2017-09-25 23:33:49 -0300

    Done

--- questions/question_21.c
+++ questions/question_21.c
@@ -4,5 +4,17 @@
 //21. Escreva um programa que leia um número e exiba se ele é positivo ou negativo
 
 main(){
+	int numero;
 	
+	printf("Digite um numero:");
+	scanf("%d", &numero);
+	
+	if(numero > 0){
+		puts("Voce digitou um numero positivo!");
+	} else 
+	if(numero < 0){
+		puts("Voce digitou um numero negativo!");
+	}else{
+		puts("Voce digitou o numero zero - 0!");
+	}	
 }
