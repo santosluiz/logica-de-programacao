54fe0025710258f9026b7de4ac225ac12ca808bb
Luiz Santos <97luizsantos@gmail.com>
2017-09-26 00:19:30 -0300

    Done

--- questions/question_44.c
+++ questions/question_44.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 44. Escreva um programa que imprima todos os números de 1  até 100, inclusive, e a soma de todos eles.
+// 44. Escreva um programa que imprima 20 números, inclusive, e a soma de todos eles.
 
 main(){
+	int vetor[20], i, soma = 0;
 	
+	for(i=0; i<20; i++){
+		printf("Digite um numero:");
+		scanf("%d", &vetor[i]);
+	}
+	
+	for(i=0; i<20; i++){				
+		printf("%d\n", vetor[i]);
+	}		
+	
+	puts("........ Soma dos valores .........");
+	
+	for(i=0; i<20; i++){				
+		soma += vetor[i];		
+	}	
+	
+	printf("%d", soma);	
 }
