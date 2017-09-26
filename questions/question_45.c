d1b881cb381b980b22f09da5ee28e7f1679a6aaa
Luiz Santos <97luizsantos@gmail.com>
2017-09-26 00:27:49 -0300

    Done

--- questions/question_45.c
+++ questions/question_45.c
@@ -4,5 +4,19 @@
 // 45. Escreva um programa que leia 5 números, e imprima a média entre eles. 
 
 main(){
+	float vetor[5], soma = 0, media = 0;
+	int i;
 	
+	for(i=0; i<5; i++){
+		printf("Digite um numero:");
+		scanf("%f", &vetor[i]);
+	}
+
+	for(i=0; i<5; i++){				
+		soma += vetor[i];		
+	}	
+	
+	media = soma/5;
+	
+	printf("%2.f", media);	
 }
