1a03bf09ad33727c47aea7e633966201e8d61dd3
Luiz Santos <97luizsantos@gmail.com>
2017-09-25 23:40:26 -0300

    Done

--- questions/question_35.c
+++ questions/question_35.c
@@ -6,5 +6,51 @@
 */
 
 main(){
+	int numero;
 	
+	printf("Digite um numero de 1 a 7:");
+	scanf("%d", &numero);
+	
+	switch(numero){
+		
+		case 1: {
+			puts("Domingo");
+			break;
+		}
+		
+		case 2:{
+			puts("Segunda-feira");
+			break;
+		}
+
+		case 3:{
+			puts("Terça-feira");
+			break;
+		}
+		
+		case 4:{
+			puts("Quarta-feira");
+			break;
+		}
+		
+		case 5:{
+			puts("Quinta-feira");
+			break;
+		}
+		
+		case 6:{
+			puts("Sexta-feira");
+			break;
+		}	
+		
+		case 7:{
+			puts("Sabado");
+			break;
+		}									
+		
+		default:{
+			puts("Numero invalido");
+			break;
+		}
+	}	
 }
