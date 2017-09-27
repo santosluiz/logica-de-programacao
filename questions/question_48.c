babb0d71ab2d923c59a9f0a152500181b81e36d1
Luiz Santos <97luizsantos@gmail.com>
2017-09-27 18:52:26 -0300

    Done

--- questions/question_48.c
+++ questions/question_48.c
@@ -4,5 +4,20 @@
 // 48.  Escreva um programa que determine se um dado número N (digitado pelo usuário) é primo ou não. 	
 
 main(){
+	int num, i, aux=0;
 	
+	printf("Digite um numero");
+	scanf("%d", &num);
+	
+	for(i=1; i<=num; i++){
+		if(num % i == 0){	
+			aux++;
+		}	
+	}
+		
+	if(aux == 2){
+		puts("e primo!");
+	} else {
+		puts("nao e primo!");
+	}	
 }
