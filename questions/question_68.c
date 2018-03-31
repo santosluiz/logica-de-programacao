b132c79bfe5c22e3d35450a870dff4048716a777
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 20:03:09 -0300

    Done

--- questions/question_68.c
+++ questions/question_68.c
@@ -2,10 +2,54 @@
 #include <stdlib.h>
 
 /* 68. Escreva um programa que leia um vetor de 15 posições de inteiros.
-	Em seguida, o programa deve ler um valor inteiro e imprimir o número de vezes que este valor ocorre no vetor.
+	Em seguida, o programa deve ler um valor inteiro e imprimir o número de vezes que este valor ocorre no vetor. */    
 
-*/    
+void insere(int *vet);
+int escolheNumero();
+void valorRepetido(int *vet, int numero);
 
 main(){
+	int vet[3];
+	int numero;
+	
+	insere(vet);
+	system("cls");
+	numero = escolheNumero();
+	valorRepetido(vet, numero);
+}
+
+void insere(int *vet){
+	int i;
+	
+	printf("Preencha o vetor com 15 inteiros: \n");
+	for(i=0; i<3; i++){
+		scanf("%d", &vet[i]);		
+	}		
+}
+
+int escolheNumero(){
+	int numero;
+	
+	printf("Digite um numero: \n");
+	scanf("%d", &numero);
+	
+	return numero;
+}
+
+
+void valorRepetido(int *vet, int numero){
+	int i, cont = 0;
+	
+	for(i=0; i<3; i++){
+		if(numero == vet[i]){
+			cont++;
+		}
+	}
+	
+	if(cont > 0){
+		printf("O inteiro  %d aparece %d vez(es) no vetor.", numero, cont);	
+	} else{
+		printf("O inteiro  %d nao aparece no vetor.", numero, cont);
+	}
 	
 }
