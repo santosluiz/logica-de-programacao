687bb801e4a5966aacbf9b8ce1bb319a352fe84e
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 20:30:50 -0300

    Done

--- questions/question_76.c
+++ questions/question_76.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* 76. Escreva um programa em C, que leia uma string, gere uma nova string com o texto invertido e imprima esta nova string.
+/* 76. Escreva um programa em C, que leia uma string, gere uma nova string com o texto invertido e imprima esta nova string. */    
 
-*/    
+int main(){
 
-main(){
+	char textoOriginal[50], copia[50];
+	int i, inverte;
+
+	printf("Digite seu nome: ");
+	gets(textoOriginal);
+
+	inverte = strlen(textoOriginal) - 1;
+		
+	for(i=0; i<strlen(textoOriginal); i++){
+		copia[inverte] = textoOriginal[i];
+		inverte--;
+	}
+
+	copia[i] = '\0';
+
+	printf("%s\n", copia);
 	
+	return 0;
+
 }
