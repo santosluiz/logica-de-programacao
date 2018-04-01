97372e7cd3383f04a7730601f1770ae782b4b560
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 21:08:54 -0300

    Done

--- questions/question_69.c
+++ questions/question_69.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 69. Escreva um programa que carregue um array com tamanho variÃ¡vel. O tamanho mÃ¡ximo do array Ã© de 100 posiÃ§Ãµes (carga de array com sentinela). 
-
-*/    
+/* 69. Escreva um programa que carregue um array com tamanho variável. O tamanho máximo do array é de 100 posições. */    
 
 main(){
+	int *array;
+	int tam;
 	
+	printf("Insira a quantidade de posicoes do array: ");
+	scanf("%d", &tam);		
+	
+	while(tam == 0 || tam > 100){
+		system("cls");
+		printf("Valor invalido.\n");		
+		printf("Insira a quantidade de posicoes do array: ");
+		scanf("%d", &tam);	
+	}
+	
+	array = (int *) malloc(tam * sizeof(int));
+	printf("Seu vetor tem: %d posicoes", tam);
 }
