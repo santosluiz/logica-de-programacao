5c3593b349aefa19176d6385e2e3b9a01d90e8c1
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 21:22:15 -0300

    Done

--- questions/question_23.c
+++ questions/question_23.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//23. Escreva um programa que leia um número e imprima se este número é ou não par.
+//23. Escreva um programa que leia um numero e imprima se este numero e ou nao par.
 
-main(){
+void verifica(int numero);
+
+int main(){
+	int numero;
 	
+	printf("Digite um numero: ");
+	scanf("%d", &numero);
+	
+	verifica(numero);
+}
+
+void verifica(int numero){
+	
+	if(numero % 2 == 0){
+		printf("%d e um numero par.", numero);
+	} else {
+		printf("%d e um numero impar.", numero);
+	}
 }
