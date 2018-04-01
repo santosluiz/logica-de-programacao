8cfdb9ee887ef1c6b2088a734312c033827d6ec9
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 21:16:51 -0300

    Done

--- questions/question_87.c
+++ questions/question_87.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#define TAM 50
+/* 87. Escreva um programa em C, que coloque a string lida toda em caixa alta. */    
 
-/* 87. Escreva um programa em C, que coloque a string lida toda em caixa alta.
 
-*/    
-
-main(){
-	
+int main(){
+  int i=0;
+  char frase[TAM];  
+  char c;
+  
+  
+  printf("Digite uma frase: ");
+  gets(frase);
+  
+  for(i=0; i<TAM; i++){
+    frase[i] = toupper(frase[i]);
+  }
+  
+  printf("%s", frase);
+  
+  return 0;
 }
