bcd745d7823d06eda0de49788227c51d9eb85b00
Luiz Santos <97luizsantos@gmail.com>
2018-03-31 21:10:01 -0300

    Done

--- questions/question_75.c
+++ questions/question_75.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 75. Escreva um programa em C, que leia uma string e conte quantas ocorrências de vogais existem nesta string.
+/* 75. Escreva um programa em C, que leia uma string e conte quantas ocorrências de vogais existem nesta string. */    
 
-*/    
+int contaVogais(char* str);
 
-main(){
+int main(){
+
+	char frase[100];
+	int qtd;
 	
+	printf("Digite uma frase: ");	
+	gets(frase);
+	
+	qtd = contaVogais(frase);
+	
+	printf("\nO numero de vogais e: %d", qtd);
+	
+	return 0;
+
+}
+
+int contaVogais(char* str){
+
+	int i=0, contador=0;
+	
+	for(i=0; i<100; i++){	
+		if((str[i]=='a') || (str[i]=='e') || (str[i]=='i') || (str[i]=='o') || (str[i]=='u') || (str[i]=='A') || (str[i]=='E') || (str[i]=='I') || (str[i]=='O') || (str[i]=='U')){	
+			contador++;	
+		}	
+	}
+	
+	return contador;
+
 }
