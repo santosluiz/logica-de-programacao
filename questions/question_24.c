e6f952d72f18c8e381c0b952dc2cf05e342dec07
Luiz Santos <97luizsantos@gmail.com>
2018-04-04 02:06:18 -0300

    Done

--- questions/question_24.c
+++ questions/question_24.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//24. Escreva um programa que leia três números e mostre o maior entre eles. 
+//24. Escreva um programa que leia tres numeros e mostre o maior entre eles. 
+int compara(int *num);
 
 main(){
+	int numeros[3];
+	int i, aux;
+	
+	printf("Digite 3 numeros: ");
+	for(i=0; i<3; i++){
+		scanf("%d", &numeros[i]);	
+	}
+	
+	aux = compara(numeros);
+
+	
+}
+
+int compara(int *num){
+	int i, aux;
+	
+	aux = num[0];
+	
+	for(i=1; i<3; i++){
+		if(aux < num[i]){
+			aux = num[i];
+		}
+	}
+	
+	printf("O numero maior e: %d", aux);
 	
 }
