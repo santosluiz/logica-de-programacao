50318c05057ffb76e22c562a1fb2c6d1a5b180d7
Julio César Da Silva <88972643+JulionBR@users.noreply.github.com>
2022-12-27 15:15:31 -0300

    done

--- questions/question_20.c
+++ questions/question_20.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //20. Uma loja vende seus produtos no sistema entrada mais duas prestações, sendo a entrada maior do que ou igual às duas prestações, as quais devem ser iguais, inteiras e as maiores possíveis. 
 // Por exemplo, se o valor da mercadoria for R$ 270,00, a entrada e as duas prestações são iguais a R$ 90,00; se o valor da mercadoria for R$ 302,75, a entrada é de R$ 102,75 e as duas prestações são a iguais a R$ 100,00. Escreva um programa que receba o valor da mercadoria e forneça o valor da entrada e das duas prestações, de acordo com as regras acima. 
 // Observe que uma justificativa para a adoção desta regra é que ela facilita a confecção e o consequente pagamento dos boletos das duas prestações
 
 main(){
-	
+	float valor, resto, entrada;
+    int parcela;
+    printf("sistema entrada mais duas prestações\n Qual é o valor da compra:");
+    scanf("%f", &valor);
+    parcela = (valor / 3);
+    resto = fmod(valor, 3);
+    entrada = parcela + resto;
+    printf("Entrada: %.2f ,duas parcela de: %d", entrada, parcela);
 }
--- questions/question_22.c
+++ questions/question_22.c
@@ -4,5 +4,16 @@
 //22. Escreva um programa que leia um número e exiba o seu módulo.
 
 main(){
-	
+    float numero;
+    printf("Digite um valor:");
+    scanf("%f", &numero);
+    if(numero > 0){
+        printf("O numero é positivo");
+    }else{
+        if(numero < 0){
+            printf("O numero é negativo");
+        }else{
+            printf("O numero é nulo");
+        }
+    }
 }
